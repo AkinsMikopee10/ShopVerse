ed71da16ac98d1342af0daa57a9dc3e7dd71064a
Michael Akinyemi <akinsmikopee2507@gmail.com>
2026-08-06 21:44:47 +0100

    implement product controller

---
+++ server/src/controllers/product.controller.js
@@ -0,0 +1,74 @@
+import productService from "../services/product.service.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+class ProductController {
+  getProducts = asyncHandler(async (req, res) => {
+    const products = await productService.getProducts(req.query);
+
+    res.status(200).json({
+      success: true,
+      message: "Products retrieved successfully.",
+      data: products,
+    });
+  });
+
+  getProductById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const product = await productService.getProductById(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Product retrieved successfully.",
+      data: product,
+    });
+  });
+
+  getProductBySlug = asyncHandler(async (req, res) => {
+    const { slug } = req.params;
+
+    const product = await productService.getProductBySlug(slug);
+
+    res.status(200).json({
+      success: true,
+      message: "Product retrieved successfully.",
+      data: product,
+    });
+  });
+
+  createProduct = asyncHandler(async (req, res) => {
+    const product = await productService.createProduct(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully.",
+      data: product,
+    });
+  });
+
+  updateProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const product = await productService.updateProduct(id, req.body);
+
+    res.status(200).json({
+      success: true,
+      message: "Product updated successfully.",
+      data: product,
+    });
+  });
+
+  deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const product = await productService.deleteProduct(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully.",
+      data: product,
+    });
+  });
+}
+
+export default new ProductController();
